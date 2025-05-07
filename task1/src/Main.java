dd0a40dfd4b4212f0afab93c5516044efc555c0f
Asmaa Hussein <asmaahlal123456789a@gmail.com>
2025-05-07 20:25:29 +0300

    Update Main.java 20231021

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("20231021-20231026-20230142");
+            System.out.println("20231021");
             System.out.println("20230142");
 
     }
